b8b37c790330d2a5c5ccc8bd37351daa1945dabb
017zontik <017zontik@gmail.com>
2020-04-05 19:54:06 +0300

    Task34

---
+++ src/src/com/task34/Task34.java
@@ -0,0 +1,51 @@
+package src.com.task34;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task34 {
+    public static void main(String[] args) {
+        ArrayList <Integer> numbers = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("numbers.txt"));
+            String line =reader.readLine();
+
+            while (line != null){
+                Pattern s = Pattern.compile("[0-9]+");
+                Matcher m = s.matcher(line);
+
+                while (m.find()) {
+                   numbers.add(Integer.parseInt(m.group())) ;
+                }
+                line = reader.readLine();
+            }
+            System.out.println(numbers);
+            reader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int sum = 0;
+        for (int i = 0; i <numbers.size() ; i++) {
+          sum += numbers.get(i);
+        }
+        System.out.println(sum);
+
+        for (int i = 0; i <numbers.size() ; i++) {
+            for (int j = i+1; j <numbers.size() ; j++) {
+                if(numbers.get(i).equals(numbers.get(j))){
+                    numbers.remove(j);
+                }
+            }
+        }
+        System.out.println(numbers);
+
+
+
+    }
+}
