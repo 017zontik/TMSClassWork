db183fbdf355eeaa1ccf29df767271e120e43f15
017zontik <017zontik@gmail.com>
2020-04-05 18:40:19 +0300

    Task 33: FileReader

---
+++ src/src/com/task33/Task33.java
@@ -0,0 +1,39 @@
+package src.com.task33;
+
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task33 {
+    public static void main(String[] args) {
+        try {
+            BufferedReader stream = new BufferedReader(new FileReader("streams.txt"));
+            String line = stream.readLine();
+            int allWordsCounter = 0;
+            int allPunctualCounter = 0;
+            while (line != null) {
+
+                int wordsCounter = line.split("[\\s]+").length;
+                allWordsCounter +=wordsCounter;
+
+                Pattern punctuation = Pattern.compile("[,\\.-:;!\\?\"\\(\\)']");
+
+                Matcher m = punctuation.matcher(line);
+                int punctualCounter = 0;
+                while (m.find()) {
+                    punctualCounter++;
+                }
+                allPunctualCounter += punctualCounter;
+                line = stream.readLine();
+
+            }
+            System.out.println("Количество слов: " + allWordsCounter );
+            System.out.println("Количество знаков препинания: " + allPunctualCounter );
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
